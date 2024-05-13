76d847c33f9a07446570cd25487745a8b1e1529a
Luis Carlos dos Santos Lima <luis.carlos.lider@hotmail.com>
2024-05-13 01:50:18 -0300

    Quarto Commit

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,6 +1,7 @@
-using BlazorApp1.Components;
+ï»¿using BlazorApp1.Components;
 using BlazorApp1.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,19 +9,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-// apos sua string de conexão estiver criada conforme acima, va para o program.cs para configirar
-// aqui vamos adicionar um serviço de contexto que irár inicializar na nossa aplicação builder.Services.AddDbContext
-// vc vai informar qual é o tipo de contexto que no caso é o AppDbContext, na sequencia passe as configurações que criamos no contexto
-// aqui será a nossa classe base e dentro do colchetes vc vai criar uma funão labda que pode chamar o de options
-//vc vai passar para a função labda o UseSqLite, e ele pede uma configuração que é a string de conexão, que nos criamos dentro do arquivo Appsettings.json com o nome de Default
-// se vc tiver varias strings de conexão 2 ou mais banco de dados, crie suas variaveis de string de coneção e passe o parâmetro para o UseSqLite
+// apos sua string de conexï¿½o estiver criada conforme acima, va para o program.cs para configirar
+// aqui vamos adicionar um serviï¿½o de contexto que irï¿½r inicializar na nossa aplicaï¿½ï¿½o builder.Services.AddDbContext
+// vc vai informar qual ï¿½ o tipo de contexto que no caso ï¿½ o AppDbContext, na sequencia passe as configuraï¿½ï¿½es que criamos no contexto
+// aqui serï¿½ a nossa classe base e dentro do colchetes vc vai criar uma funï¿½o labda que pode chamar o de options
+//vc vai passar para a funï¿½ï¿½o labda o UseSqLite, e ele pede uma configuraï¿½ï¿½o que ï¿½ a string de conexï¿½o, que nos criamos dentro do arquivo Appsettings.json com o nome de Default
+// se vc tiver varias strings de conexï¿½o 2 ou mais banco de dados, crie suas variaveis de string de coneï¿½ï¿½o e passe o parï¿½metro para o UseSqLite
 
-var connectionStr = builder.Configuration.GetConnectionString("Default");  //builder.Configuration.GetConnectionString pega a string de coneção que vc criou dentro do seu appsettings.json
+var connectionStr = builder.Configuration.GetConnectionString("Default");  //builder.Configuration.GetConnectionString pega a string de coneï¿½ï¿½o que vc criou dentro do seu appsettings.json
 builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlite(connectionStr));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();;
 //builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlite(builder.Configuration.GetConnectionString("Default")));
 //builder.Services.AddDbContext<AppDbContext>(x => x.UseInMemoryDatabase(databaseName: "app"));
 
-// Agora vamos criar os migrations, cada modificação no banco o migrations irá refletir a alteração ou criação no banco de dados, ele cria uma linha do tempo como versões do seu banco de dados, em cada versão uma alteração que vc criou
+// Agora vamos criar os migrations, cada modificaï¿½ï¿½o no banco o migrations irï¿½ refletir a alteraï¿½ï¿½o ou criaï¿½ï¿½o no banco de dados, ele cria uma linha do tempo como versï¿½es do seu banco de dados, em cada versï¿½o uma alteraï¿½ï¿½o que vc criou
 
 
 var app = builder.Build();
