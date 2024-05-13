16c946e1c7447cb93f7b8d8ae606907e0d112b27
Luis Carlos dos Santos Lima <luis.carlos.lider@hotmail.com>
2024-05-13 01:37:59 -0300

    Terceiro Commit

--- BlazorApp1/Models/Todo.cs
+++ BlazorApp1/Models/Todo.cs
@@ -6,7 +6,7 @@ namespace BlazorApp1.Models;
 
 public class Todo
 {
-    [Key] //Atrioto Key é campo de chave primaria que será criado no banco de dados
+    //[Key] //Atrioto Key é campo de chave primaria que será criado no banco de dados
     public int Id { get; set; }
     //[Required] //Atrioto Required é campo requerido que será criado no banco de dados, vamos definir essas validações em outro local, não aqui no model mas é possivel deixar aqui dependendo da sua arquitetura do programa
     public DateTime CreationDate { get; set; }
@@ -15,7 +15,6 @@ public class Todo
     public string? Description { get; set; }
     public bool Done { get; set; }
     public DateTime? DoneDate { get; set; }
-
     public int CategoryId { get; set; }
     public Category? Category { get; set; }
 }
