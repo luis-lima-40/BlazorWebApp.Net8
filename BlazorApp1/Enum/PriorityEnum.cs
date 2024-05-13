981bad47040a2019e04c0b36510ff408f901eee0
Luis Carlos dos Santos Lima <luis.carlos.lider@hotmail.com>
2024-05-13 10:30:48 -0300

    Quinto Commit

--- BlazorApp1/Enum/PriorityEnum.cs
+++ BlazorApp1/Enum/PriorityEnum.cs
@@ -1,4 +1,11 @@
-﻿namespace BlazorApp1.Enum;
+﻿using System.Text.Json.Serialization;
+
+namespace BlazorApp1.Enum;
+
+
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+//para que vc transforme um enum em uma string, uma linha antes do public enum  use [JsonConverter(typeof(JsonStringEnumConverter))], faça a importação do using System.Text.Json.Serialization;
 
 public enum TodoPriority
 {
--- BlazorApp1/Models/Todo.cs
+++ BlazorApp1/Models/Todo.cs
@@ -6,11 +6,12 @@ namespace BlazorApp1.Models;
 
 public class Todo
 {
-    //[Key] //Atrioto Key é campo de chave primaria que será criado no banco de dados
+    [Key] //Atrioto Key é campo de chave primaria que será criado no banco de dados
     public int Id { get; set; }
     //[Required] //Atrioto Required é campo requerido que será criado no banco de dados, vamos definir essas validações em outro local, não aqui no model mas é possivel deixar aqui dependendo da sua arquitetura do programa
     public DateTime CreationDate { get; set; }
-    public TodoPriority Priority { get; set; }
+    //public TodoPriority Priority { get; set; }
+    public int Priority { get; set; }
     public string? Title { get; set; }
     public string? Description { get; set; }
     public bool Done { get; set; }
